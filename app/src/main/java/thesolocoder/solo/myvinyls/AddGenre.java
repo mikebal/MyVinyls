39d1a898a5bf73abbfe7db00995a4ee74a86aebd
Michael <darekmichael@gmail.com>
2016-01-18 12:46:58 -0700

    Modifications so that when clicking down on a category list it opens a diffrent window.
    Added icon for expand_less

--- app/src/main/java/thesolocoder/solo/myvinyls/AddGenre.java
+++ app/src/main/java/thesolocoder/solo/myvinyls/AddGenre.java
@@ -2,14 +2,26 @@ package thesolocoder.solo.myvinyls;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+// TO-DO
+// When expanded category hit: - Cut the current list view to end and click possition
+//                             - Show checkbox list for that category
+//                             - Create populate secondary listView with the remainder of the first list view
+//                             - On click outside of Step 2 (dynamic check box area) recreate single list;
 
 public class AddGenre extends AppCompatActivity {
 
     ListView genre_list;
+    LinearLayout selectedView;
+    TextView selectedCategoryTextView;
+    ImageButton upArrow;
     private static final String PRIMARY_GENRE_FILE = "#genre.txt";
 
     @Override
@@ -22,15 +34,30 @@ public class AddGenre extends AppCompatActivity {
 
     private void setupVariables(){
         genre_list = (ListView) findViewById(R.id.listView_genre);
+        selectedView = (LinearLayout) findViewById(R.id.LinearLayoutSelectedCategory);
+        selectedCategoryTextView = (TextView) findViewById(R.id.selectedCateogryTextView);
+        upArrow = (ImageButton) findViewById(R.id.imageButton_genre_pullUp);
     }
     private void populateList(){
 
-        ArrayList<String> primaryGenres; 
+        ArrayList<String> primaryGenres;
         GenreFileManager fileManager = new GenreFileManager(getApplicationContext());
 
         primaryGenres = fileManager.readInGenres(PRIMARY_GENRE_FILE);
 
-        GenreAdapter customAdapter = new GenreAdapter(this, primaryGenres);
+        GenreAdapter customAdapter = new GenreAdapter(this, primaryGenres, selectedView, selectedCategoryTextView);
         genre_list.setAdapter(customAdapter);
+
     }
+
+    public void closeSelectedCategoryView(View view){
+        selectedView.setVisibility(View.GONE);
+        genre_list.setVisibility(View.VISIBLE);
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/thesolocoder/solo/myvinyls/GenreAdapter.java
+++ app/src/main/java/thesolocoder/solo/myvinyls/GenreAdapter.java
@@ -5,18 +5,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-        import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class GenreAdapter  extends BaseAdapter{
 
     private LayoutInflater inflator;
     private ArrayList<String> genreCategories;
+    LinearLayout selectedLayout;
+    TextView selectedTextView;
 
-    public GenreAdapter(Context context, ArrayList<String> data) {
+    public GenreAdapter(Context context, ArrayList<String> data, LinearLayout selectedLayout, TextView slectedTextView) {
         inflator = LayoutInflater.from(context);
         this.genreCategories = data;
+        this.selectedLayout = selectedLayout;
+        this.selectedTextView = slectedTextView;
     }
 
     public int getCount() {
@@ -32,23 +38,27 @@ public class GenreAdapter  extends BaseAdapter{
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, final ViewGroup parent) {
         //return super.getView(position, convertView, parent);
 
-        View customView = inflator.inflate(R.layout.genrelistview, parent, false);
-
-
+        final View customView = inflator.inflate(R.layout.genrelistview, parent, false);
         TextView category = (TextView) customView.findViewById(R.id.genreMainCat);
-       // TextView body = (TextView) customView.findViewById(R.id.list_textview_DISC);
-      //  TextView cost = (TextView) customView.findViewById(R.id.list_textview_COST);
-
         String item = getItem(position);
-
         category.setText(item);
-        //body.setText(item.getDiscription());
-      //  cost.setText(item.getCost());
 
-        return customView;
+        ImageButton dropDown = (ImageButton) customView.findViewById(R.id.imageButton_genre_dropdown);
+
+            dropDown.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                   selectedTextView.setText(genreCategories.get(position));
+                   parent.setVisibility(View.GONE);
+                   selectedLayout.setVisibility(View.VISIBLE);
+                }
+            });
+
+            return customView;
 
     }
 }
