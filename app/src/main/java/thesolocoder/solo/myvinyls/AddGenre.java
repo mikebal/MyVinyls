869921fb5708557be38e67f94b7d5c2a0ea736c3
Michael <darekmichael@gmail.com>
2016-01-21 23:05:57 -0700

    Attempting to rebase

--- app/src/main/java/thesolocoder/solo/myvinyls/AddGenre.java
+++ app/src/main/java/thesolocoder/solo/myvinyls/AddGenre.java
@@ -1,10 +1,14 @@
 package thesolocoder.solo.myvinyls;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -86,9 +90,34 @@ public class AddGenre extends AppCompatActivity {
         //selectedCategoryTextView.setVisibility(View.VISIBLE);
     }
 
+    public void addNewGenre(View v){
 
 
+            final AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
+            alert.setTitle("Adding Genre!");
+            alert.setMessage("Enter new Genre:");
 
+            final EditText input = new EditText(this);
+
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        input.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            alert.setView(input);
+
+            alert.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = input.getText().toString();
+                    if (!value.equals("")) {
+                        GenreFileManager genreManager = new GenreFileManager(getApplicationContext());
+                        genreManager.addAlphabetically(value, "#genre.txt");
+
+                    } else
+                        finish();
+
+                }
+            });
+            alert.create();
+            alert.show();
+    }
 
 }
